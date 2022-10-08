aef420a8298268aaec8e8fe449fc6dca0278bb63
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-10-08 21:51:50 +0100

    import ChatEngine and add styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <ChatEngine 
+        height="100vh"
+        projectID="20ad6db0-1d58-44a0-ab65-8486b44cfb34"
+        userName="John"
+        userSecret="test123"
+      />
     </div>
   );
 }
