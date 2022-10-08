e6126d8044bcdfad079e01f3843791b6573d82fc
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-10-08 22:22:49 +0100

    create renderMessages

--- src/components/chatFeed/chatFeed.component.jsx
+++ src/components/chatFeed/chatFeed.component.jsx
@@ -1,6 +1,34 @@
+import MyMessage from '../myMessage/myMessage.component';
+import TheirMessage from '../theirMessage/theirMessage.component';
+import MessageForm from '../messageForm/messageForm.component';
+
 const ChatFeed = (props) => {
 
+    const { chats, activeChat, userName, messages } = props;
 
+    const chat = chats && chats[activeChat];
+
+    const renderMessages = () => {
+        const keys = Object.keys(messages);
+
+        return keys.map( (key, index) => {
+            const message = messages[key];
+            const lastMessageKey = index === 0 ? null : keys[index - 1];
+            const isMyMessage = userName === message.sender.userName;
+
+            return (
+                <div key={`msg_${index}`} style={{ width: '100%' }}>
+                    <div className="message-block">
+                        {
+                            isMyMessage ? <MyMessage message={message} /> : <TheirMessage message={message} lastMessage={messages[lastMessageKey]} />
+                        }
+                    </div>
+
+                    <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px' }}></div>
+                </div>
+            )
+        })
+    }
 
     return (
         <div>
