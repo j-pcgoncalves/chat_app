7123f89207aec5f3cf6f31204565f5ea3dae646a
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-10-09 01:50:23 +0100

    fixing bug

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import ChatFeed from './components/chatFeed/chatFeed.component';
 import LoginForm from './components/loginForm/loginForm.component';
 import './App.css';
 
+const projectID = '20ad6db0-1d58-44a0-ab65-8486b44cfb34';
+
 const App = () => {
   
   if (!localStorage.getItem('username')) return <LoginForm />;
@@ -11,9 +13,9 @@ const App = () => {
     <div className="App">
       <ChatEngine 
         height="100vh"
-        projectID="20ad6db0-1d58-44a0-ab65-8486b44cfb34"
-        userName="John"
-        userSecret="test123"
+        projectID={projectID}
+        userName={localStorage.getItem('username')}
+        userSecret={localStorage.getItem('password')}
         renderChatFeed={ (chatAppProps) => <ChatFeed {...chatAppProps } />}
         onNewMessage = { () => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play() }
       />
